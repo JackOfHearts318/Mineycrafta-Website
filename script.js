ab4eff2cffde430ddc7aa521013ae6b8d9f01300
JackOfHearts318 <45011748+JackOfHearts318@users.noreply.github.com>
2019-12-09 09:49:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
-//when the button that corresponds with this code is clicked, everything affected will change to "Nether"
+//When the button that corresponds with this code is clicked, everything affected will change to "Nether!"
 function changeNether() {
    document.getElementById("title").style.backgroundColor = "#B22222";
    document.getElementById("containa").style.backgroundColor = "#800000";
    document.getElementById("baybee").style.backgroundColor = "#783c00";
    document.getElementById("containa2").style.backgroundColor = "#800000";
    document.getElementById("baybee2").style.backgroundColor = "#783c00";
-   document.getElementById("iMage1").src = "images/netherportal.jpg";
-   document.getElementById("iMage2").src = "images/nether1.jpg";
-   document.getElementById("iMage3").src = "images/netherfortress.jpg";
-   document.getElementById("iMage4").src = "images/netherfortress2.jpg";
-   document.getElementById("iMage5").src = "images/netherwart.jpg";
-   document.getElementById("iMage6").src = "images/nethermobs.jpg";
-   document.getElementById("iMage7").src = "images/netherghast.jpg";
-   document.getElementById("iMage8").src = "images/netherupdate.jpg";
+   document.getElementById("iMage1").src = "netherportal.jpg";
+   document.getElementById("iMage2").src = "nether1.jpg";
+   document.getElementById("iMage3").src = "netherfortress.jpg";
+   document.getElementById("iMage4").src = "netherfortress2.jpg";
+   document.getElementById("iMage5").src = "netherwart.jpg";
+   document.getElementById("iMage6").src = "nethermobs.jpg";
+   document.getElementById("iMage7").src = "netherghast.jpg";
+   document.getElementById("iMage8").src = "netherupdate.jpg";
    document.getElementById('click').play();
 }
 
-//when the button that corresponds with this code is clicked, everything affected will change to "Overworld"
+//When the button that corresponds with this code is clicked, everything affected will change to "Overworld!"
 function changeOverworld() {
 	document.getElementById("title").style.backgroundColor = "green";
 	document.getElementById("containa").style.backgroundColor = "#655421";
 	document.getElementById("baybee").style.backgroundColor = "#755553";
 	document.getElementById("containa2").style.backgroundColor = "#655421";
 	document.getElementById("baybee2").style.backgroundColor = "#755553";
-	document.getElementById("iMage1").src = "images/overworld1copy.jpg";
-	document.getElementById("iMage2").src = "images/village.jpg";
-	document.getElementById("iMage3").src = "images/swamp.jpg";
-	document.getElementById("iMage4").src = "images/mobs.jpg";
-	document.getElementById("iMage5").src = "images/desertsavannah.jpg";
-	document.getElementById("iMage6").src = "images/desertvillagemesa.jpg";
-	document.getElementById("iMage7").src = "images/mountains.jpg";
-	document.getElementById("iMage8").src = "images/cave.jpg";
+	document.getElementById("iMage1").src = "overworld1copy.jpg";
+	document.getElementById("iMage2").src = "village.jpg";
+	document.getElementById("iMage3").src = "swamp.jpg";
+	document.getElementById("iMage4").src = "mobs.jpg";
+	document.getElementById("iMage5").src = "desertsavannah.jpg";
+	document.getElementById("iMage6").src = "desertvillagemesa.jpg";
+	document.getElementById("iMage7").src = "mountains.jpg";
+	document.getElementById("iMage8").src = "cave.jpg";
 	document.getElementById('click').play();
 }
 
-//when the button that corresponds with this code is clicked, everything affected will change to "End"
+//When the button that corresponds with this code is clicked, everything affected will change to "End!"
 function changeEnd() {
 	document.getElementById("title").style.backgroundColor = "black";
 	document.getElementById("containa").style.backgroundColor = "#4d004d";
 	document.getElementById("baybee").style.backgroundColor = "#FEFCD7";
 	document.getElementById("containa2").style.backgroundColor = "#4d004d";
 	document.getElementById("baybee2").style.backgroundColor = "#FEFCD7";
-	document.getElementById("iMage1").src = "images/endportal.jpg";
-	document.getElementById("iMage2").src = "images/endcity.jpg";
-	document.getElementById("iMage3").src = "images/otherendportal.jpg";
-	document.getElementById("iMage4").src = "images/finalboss.jpg";
-	document.getElementById("iMage5").src = "images/endcity2.jpg";
-	document.getElementById("iMage6").src = "images/endislands.jpg";
-	document.getElementById("iMage7").src = "images/enderdragonegg.jpg";
-	document.getElementById("iMage8").src = "images/dragonheaddragonbreath.jpg";
+	document.getElementById("iMage1").src = "endportal.jpg";
+	document.getElementById("iMage2").src = "endcity.jpg";
+	document.getElementById("iMage3").src = "otherendportal.jpg";
+	document.getElementById("iMage4").src = "finalboss.jpg";
+	document.getElementById("iMage5").src = "endcity2.jpg";
+	document.getElementById("iMage6").src = "endislands.jpg";
+	document.getElementById("iMage7").src = "enderdragonegg.jpg";
+	document.getElementById("iMage8").src = "dragonheaddragonbreath.jpg";
 	document.getElementById('click').play();
 }
+
+//Look for the Gummibär Album in stores on November 13th, with lots of music, videos, and extras.
